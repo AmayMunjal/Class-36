6544c7a077b915716fd449b2fb68d80ceb60ca4a
AmayMunjal <74940949+AmayMunjal@users.noreply.github.com>
2021-04-03 19:02:26 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var playerCount,gameState = 0, database;
+var position;
+var form,player,game
+
+
+function setup(){
+  database = firebase.database();
+  createCanvas(500,500);
+
+
+}
+
+function draw(){
+  background("white");
+  
+
+  
+}
+
